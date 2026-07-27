b481586674381f505ff51e8ab34737d7da1fd10b
Zeliha <zel.celebi@gmail.com>
2026-07-27 16:13:00 +0200

    feat: initialize Clean Architecture project with EF Core

---
+++ RecruitFlow.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitFlow.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ RecruitFlow.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecruitFlow.Domain.Entities;
+
+
+namespace RecruitFlow.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; } = null!;
+
+        public DbSet<JobPosition> JobPositions { get; set; } = null!;
+
+        public DbSet<Candidate> Candidates { get; set; } = null!;
+    }
+}
